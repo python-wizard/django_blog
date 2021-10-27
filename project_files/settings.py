b6075463489eabc2b4b312a78d3eb9d916d3a674
Python Wizard <max.python.wizard@gmail.com>
2021-10-27 19:26:03 +0200

    project using dotenv and .env files to load enviromental variables

--- project_files/settings.py
+++ project_files/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('.')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
